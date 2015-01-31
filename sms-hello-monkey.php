76ffc02ddd10cc85d8526ab5699489656e76b871
k-dang <dangkevin12@gmail.com>
2015-01-31 10:56:44 -0500

    new

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -1,7 +1,4 @@
 <?php
-
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
