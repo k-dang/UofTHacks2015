1003ba1e44b71b2749227886a2f58225437cf352
k-dang <dangkevin12@gmail.com>
2015-01-31 10:54:20 -0500

    new

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -1,20 +1,26 @@
 <?php
-	include("functions.php");
+
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
- 	$var = (String) $_REQUEST['Body'];
-	// // Getting Facebook Data
-	$a = get_stock_data_from_yahoo_finance_pv($var, $e);
+	
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$curl = curl_init();
+		// Set some options - we are passing in a useragent too here
+	curl_setopt_array($curl, array(
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL => 'uofthackstwil.herokuapp.com/stocks/1',
+	    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+	));
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+	// Close request to clear up some resources
+	curl_close($curl);
 
-	// if ($a != -1) {
-	//     // echo "<pre>";
-	//     // print_r($a);
-	//     // echo "</pre>";
-	// } else
-	//     // echo "No stock data is available. The detail of the error is: $e";
+	$results = json_decode($resp);
 
 
 ?>
 <Response>
-    <Message>Hello my name is, <?php echo $a["k4"] ?></Message>
+    <Message>Hello my name is, <?php echo $results ?></Message>
 </Response>
