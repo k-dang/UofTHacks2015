5bbe1c19fe9daaa009574c438654abb8904f40a3
k-dang <dangkevin12@gmail.com>
2015-02-01 01:00:48 -0500

    fk

--- app/controllers/StockYahooApiController.php
+++ app/controllers/StockYahooApiController.php
@@ -77,8 +77,7 @@ class StockYahooApiController extends \BaseController {
 		curl_setopt_array($curl, array(
 		    CURLOPT_RETURNTRANSFER => 1,
 		    //CURLOPT_URL => 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-		   	CURLOPT_URL => 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22'.$name.'%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-
+		   	CURLOPT_URL => 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20('.$name.')&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
 		    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
 		));
 
